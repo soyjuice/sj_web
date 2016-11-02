c919e5c7135f8946a82c4b2c6810885eab9eb87c
soyui <1316692979@qq.com>
2016-11-02 19:38:51 +0800

    Site updated: 2016-11-02 19:38:35

--- js/search.js
+++ js/search.js
@@ -1,1 +1,71 @@
-function getParameterByName(e,r){r||(r=window.location.href),e=e.replace(/[\[\]]/g,"\\$&");var n=new RegExp("[?&]"+e+"(=([^&#]*)|&|#|$)"),t=n.exec(r);return t?t[2]?decodeURIComponent(t[2].replace(/\+/g," ")):"":null}function calcScore(e,r){for(var n=0,t=0;t<r.length&&null!=r[t];++t)for(var o=0;o<e.length;++o)for(var a=0;a<r[t].length;++a)r[t][a]==e[o]&&n++;return n}function addCountToJson(e,r){for(var n=0;n<r.length;++n)r[n].score=calcScore(e,r[n].content_tokenize)+100*calcScore(e,r[n].title_tokenize)}function quickSort(e){if(e.length<=1)return e;for(var r=Math.floor(e.length/2),n=e.splice(r,1)[0],t=[],o=[],a=0;a<e.length;a++)e[a].score<n.score?t.push(e[a]):o.push(e[a]);return quickSort(t).concat(n,quickSort(o))}window.onload=function(){var e=getParameterByName("query");null==e&&(e=""),$(".st-default-search-input").val(e),$.get("../search.json",function(r){addCountToJson(e,r),r=quickSort(r);for(var n=r.length-1;n>=0;--n)$("#result").append('<a href="'+r[n].url+'" target="_blank"><li>'+r[n].title+" 评分: "+r[n].score+"</li></a>")})};
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function calcScore(key, tokenize) {
+    var score = 0;
+    for (var x = 0; x < tokenize.length; ++x) {
+        if (tokenize[x] == null) {
+            break;
+        }
+        for (var i = 0; i < key.length; ++i) {
+            for (var t = 0; t < tokenize[x].length; ++t) {
+                if (tokenize[x][t] == key[i]) {
+                    score++;
+                }
+            }
+
+        }
+    }
+    return score;
+}
+
+function addCountToJson(key, json) {
+    for (var i = 0; i < json.length; ++i) {
+        // 题目匹配权重100 , 内容匹配权重1 , 匹配算法: 单字匹配 (待优化)
+        json[i]['score'] = calcScore(key, json[i].content_tokenize) + calcScore(key, json[i].title_tokenize) * 100;
+    }
+}
+
+
+// 按 json 元素 score 值进行排序
+function quickSort(json) {
+    if (json.length <= 1) {
+        return json;
+    }
+    var pivotIndex = Math.floor(json.length / 2);
+    var pivot = json.splice(pivotIndex, 1)[0]; // 基准
+    var left = [];
+    var right = [];
+    for (var i = 0; i < json.length; i++) {
+        if (json[i].score < pivot.score) {
+            left.push(json[i]);
+        } else {
+            right.push(json[i]);
+        }
+    }
+    return quickSort(left).concat(pivot, quickSort(right));
+}
+
+window.onload = function () {
+
+    var query = getParameterByName('query');
+    if (query == null) {
+        query = '';
+    }
+
+    $(".st-default-search-input").val(query);
+    $.get("../search.json", function (result) {
+        addCountToJson(query, result);
+        result = quickSort(result);
+        for (var i = result.length - 1; i >= 0; --i) {
+            $("#result").append('<a href="' + result[i].url + '"' + ' target="_blank"' + '><li>' + result[i].title /*+ ' 评分: ' + result[i].score-->*/ + '</li></a>');
+        }
+    });
+};
