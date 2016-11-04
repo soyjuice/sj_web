b1c75b63e2c9a99466da686902d321054ff293ba
soyui <1316692979@qq.com>
2016-11-04 11:44:15 +0800

    Site updated: 2016-11-04 11:44:01

---
+++ js/merge.js
@@ -0,0 +1,12 @@
+function merge_code(a)
+{
+	document.getElementById('code_pre'+a).style.display="block";
+	document.getElementById('code_re'+a).style.display="none";
+	document.getElementById('code_set'+a).style.display="none";
+}
+function open_code(a)
+{
+	document.getElementById('code_pre'+a).style.display="none";
+	document.getElementById('code_re'+a).style.display="block";
+	document.getElementById('code_set'+a).style.display="block";
+}
